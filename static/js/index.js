264539c780e82e92d23f340951b7b9b50c470049
luca-luna <70532596+luca-luna@users.noreply.github.com>
2022-12-01 22:07:44 -0500

    softly tested route generation works

--- static/js/index.js
+++ static/js/index.js
@@ -1,68 +1,50 @@
-function clearBuildings(svgDoc) {
-    svgDocsvgDoc.getElementById('davis').style.fill = 'none';
-    svgDocsvgDoc.getElementById('cook').style.fill = 'none';
-    svgDocsvgDoc.getElementById('jacobs').style.fill = 'none';
-    svgDocsvgDoc.getElementById('su').style.fill = 'none';
-}
-
-
 $(document).ready(function() {
     let svg = document.getElementById('map');
     console.log(svg);
-
-    svg.addEventListener("load", function () {
-	alert("hi");
-	console.log("HIII");
-    });
     
     let svgDoc = svg.contentDocument;
 
-    let davis = svgDoc.getElementById("davis");
-    davis.style.stroke = "black";
-
-    let jacobs = svgDoc.getElementById("jacobs");
-    jacobs.style.stroke = "black";
-
-    let su = svgDoc.getElementById("su");
-    su.style.stroke = "black";
-
-    let cook = svgDoc.getElementById("cook");
-    cook.style.stroke = "black";
-    
+    // color start and end buildings before route generation
     $('select').on('change', function() {
-	let building = svgDoc.getElementById(this.value);
+	let new_building = svgDoc.getElementById(this.value);
 	let previous = svgDoc.getElementById(this.data);
 
-	(this.id == "start") ? building.style.fill = "red" : building.style.fill = "green";
+	(this.id == "start") ? new_building.style.fill = "red" : new_building.style.fill = "green";
 	(previous != null) ? previous.style.fill = "none" : 1;
 
 	this.data = this.value;
     });
 
-
+    //generate route
     $('.btn').click(function() {
-	let start = $('#start').val();
-	let end = $('#end').val();
-	console.log(start);
-	console.log(end);
 
-	// color path
-	let path = svgDoc.getElementById(`${start[0]}-${end[0]}`);
-	(path == null) ? path = svgDoc.getElementById(`${end[0]}-${start[0]}`) : 1;
-	path.style.opacity = 1;
+	try {	
+	    let start = $('#start').val();
+	    let end = $('#end').val();
 
-	// color start and end buildings
-	svgDoc.getElementById(start).style.fill = "red";
-	svgDoc.getElementById(end).style.fill = "green";
+	    let path = svgDoc.getElementById(`${start[0]}-${end[0]}`);
+	    (path == null) ? path = svgDoc.getElementById(`${end[0]}-${start[0]}`) : 1;
+	    (path != null) ? path.style.opacity = 1 : 1;
 
-	$('select').on('change', function() {
-	    path.style.opacity = 0;
-	    
-	    //let building = svgDoc.getElementById(this.value);
-	    //let previous = svgDoc.getElementById(this.data);
-	    //previous.style.fill = "none";
-	    //(this.id == "start") ? building.style.fill = "red" : building.style.fill = "green";
-	});
+	    // color start and end buildings after route generation
+	    svgDoc.getElementById(start).style.fill = "red";
+	    svgDoc.getElementById(end).style.fill = "green";
+
+	    $('select').on('change', function() {
+		(path != null) ? path.style.opacity = 0 : 1;
+		
+		let building = svgDoc.getElementById(this.value);
+		let previous = svgDoc.getElementById(this.data);
+		previous.style.fill = "none";
+		(this.id == "start") ? building.style.fill = "red" : building.style.fill = "green";
+
+		this.data = this.value;
+	    });
+
+	}
+	catch {
+	    alert("Please select start and end locations");
+	}
 	
     });
     
