859b5eef4ea089797cf8513185dcbfe29ab8e464
luca-luna <70532596+luca-luna@users.noreply.github.com>
2022-12-01 21:32:58 -0500

    path works but need to uncolor old building on route change

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,11 @@
+function clearBuildings(svgDoc) {
+    svgDocsvgDoc.getElementById('davis').style.fill = 'none';
+    svgDocsvgDoc.getElementById('cook').style.fill = 'none';
+    svgDocsvgDoc.getElementById('jacobs').style.fill = 'none';
+    svgDocsvgDoc.getElementById('su').style.fill = 'none';
+}
+
+
 $(document).ready(function() {
     let svg = document.getElementById('map');
     console.log(svg);
@@ -29,6 +37,33 @@ $(document).ready(function() {
 	(previous != null) ? previous.style.fill = "none" : 1;
 
 	this.data = this.value;
+    });
+
+
+    $('.btn').click(function() {
+	let start = $('#start').val();
+	let end = $('#end').val();
+	console.log(start);
+	console.log(end);
+
+	// color path
+	let path = svgDoc.getElementById(`${start[0]}-${end[0]}`);
+	(path == null) ? path = svgDoc.getElementById(`${end[0]}-${start[0]}`) : 1;
+	path.style.opacity = 1;
+
+	// color start and end buildings
+	svgDoc.getElementById(start).style.fill = "red";
+	svgDoc.getElementById(end).style.fill = "green";
+
+	$('select').on('change', function() {
+	    path.style.opacity = 0;
+	    
+	    //let building = svgDoc.getElementById(this.value);
+	    //let previous = svgDoc.getElementById(this.data);
+	    //previous.style.fill = "none";
+	    //(this.id == "start") ? building.style.fill = "red" : building.style.fill = "green";
+	});
+	
     });
     
 });
