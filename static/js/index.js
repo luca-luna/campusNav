6af403ba56e9e93d3dfe1a99cee0c85c3efbb4ec
sean <shayanaf@buffalo.edu>
2022-12-05 19:15:21 -0500

    start travel button

--- static/js/index.js
+++ static/js/index.js
@@ -8,14 +8,14 @@ document.addEventListener("DOMContentLoaded", function () {
 	    let new_building = svgDoc.getElementById(this.value);
 	    let previous = svgDoc.getElementById(this.data);
 
-	    (this.id == "start") ? new_building.style.fill = "red" : new_building.style.fill = "green";
+	    (this.id == "start") ? new_building.style.fill = "rgba(220, 53, 69, 0.5)" : new_building.style.fill = "rgba(25, 135, 84, 0.5)";
 	    (previous != null) ? previous.style.fill = "none" : 1;
 
 	    this.data = this.value;
 	});
 
 	//generate route
-	$('.btn').click(function() {
+	$('.nav-btn').click(function() {
 
 	    try {	
 		let start = $('#start').val();
@@ -26,8 +26,10 @@ document.addEventListener("DOMContentLoaded", function () {
 		(path != null) ? path.style.opacity = 1 : 1;
 
 		// color start and end buildings after route generation
-		svgDoc.getElementById(start).style.fill = "red";
-		svgDoc.getElementById(end).style.fill = "green";
+		svgDoc.getElementById(start).style.fill = "rgba(220, 53, 69, 0.5)";
+		svgDoc.getElementById(end).style.fill = "rgba(25, 135, 84, 0.5)";
+
+		document.getElementById("putBtnHere").innerHTML = '<button type="button" class="btn btn-success start-travel">Start Travel</button>'
 
 		$('select').on('change', function() {
 		    (path != null) ? path.style.opacity = 0 : 1;
@@ -35,9 +37,13 @@ document.addEventListener("DOMContentLoaded", function () {
 		    let building = svgDoc.getElementById(this.value);
 		    let previous = svgDoc.getElementById(this.data);
 		    previous.style.fill = "none";
-		    (this.id == "start") ? building.style.fill = "red" : building.style.fill = "green";
+		    (this.id == "start") ? building.style.fill = "rgba(220, 53, 69, 0.5)" : building.style.fill = "rgba(25, 135, 84, 0.5)";
 
 		    this.data = this.value;
+
+			document.getElementById("putBtnHere").innerHTML = '<button type="button" class="btn btn-success nav-btn">Begin Navigation </button>'
+			
+
 		});
 
 	    }
