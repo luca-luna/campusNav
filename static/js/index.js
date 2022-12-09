62a3e096ebe7f84c3dc145cd11e9a0bf95acc1e0
Thy <ngocanht@buffalo.com>
2022-12-08 22:58:16 -0500

    Added instructions

--- static/js/index.js
+++ static/js/index.js
@@ -95,7 +95,124 @@ document.addEventListener("DOMContentLoaded", function () {
 	//DEFINE TEXT INSTUCTIONS HERE AS VARIABLES (e.g. Davis to SU is one variable)
 	$('.start-travel').click(function() {
 		const label = document.getElementById('directions');
-		label.textContent = 'Replacement label text';
+		var start = document.getElementById('start').value;
+		var end = document.getElementById('end').value;
+		
+		//DIRECT PATH DIRECTIONS
+		if ($('input[name=type]:checked').val() == "direct") {
+		    switch (start){
+				case "cook":
+					switch (end){
+						case "jacobs":
+							label.textContent = 'Head toward Capen Hall and keep going straight to O\'Brian Hall.\nKeep on the right side of the building.\nCross the street and you should arrive at Jacobs Management Center';
+							break;
+						case "su":
+							label.textContent = 'Head toward Capen Hall and keep going straight to O\'Brian Hall.\nKeep on the left side of the building.\nKeep going straight and you should see the Student Union on your left.';
+							break;
+						case "davis":
+							label.textContent = 'Head toward Capen Hall and turn left between Talbert Hall and Capen Hall and go down the stairs.\nTurn right and follow the sidewalk till you see Bonner hall and cross the street.\nDavis Hall should be right ahead';
+							break;
+					}
+					break;
+				case "jacobs":
+					switch (end){
+						case "cook":
+							label.textContent = 'Exit Jacobs Management Center and cross the street.\nTurn to the left and walk straight past Capen Hall.\nCooke Hall is to your left.';
+							break;
+						case "su":
+							label.textContent = 'Exit Jacobs Management Center and cross the street.\nGo between Baldy Hall and O\'Brian Hall.\nThe Student Union is to your right.';
+							break;
+						case "davis":
+							label.textContent = 'Exit Jacobs Management Center and cross the street.\nGo between Baldy Hall and O\'Brian Hall and head down the stairs ahead.\nGo through the plaza and cross the street.\nDavis Hall should be right ahead.';
+							break;
+					}
+					break;
+				case "su":
+					switch (end){
+						case "cook":
+							label.textContent = 'Exit the Student Union on the second floor.\nYou should see Lockwood Library and Baldy Hall in front of you.\nTurn left and head straight.\nKeep on the left and walk past Capen Hall.\nCooke Hall should be on your left.';
+							break;
+						case "jacobs":
+							label.textContent = 'Exit the Student Union on the second floor.\nYou should see Lockwood Library and Baldy Hall in front of you.\nTurn right and head to Baldy Hall.\nGo between Baldy Hall and O\'Brian Hall and you should see Jacobs Management Center across the street.';
+							break;
+						case "davis":
+							label.textContent = 'Exit the Student Union from the first floor.\nYou should see The Commons in front of you.\nTurn left and follow the sidewalk till you reach Bonner Hall. \nTurn right and cross the street.\nDavis Hall should be right ahead.';
+							break;
+					}
+					break;
+				case "davis":
+					switch (end){
+						case "cook":
+							label.textContent = 'Exit the building and cross the street toward Bonner Hall.\nTurn right and follow the sidewalk till you pass Capen Hall and see a staircase.\nHead up the stair case and turn right after passing the overhead.\nCooke Hall is to your left.';
+							break;
+						case "jacobs":
+							label.textContent = 'Exit the building and cross the street toward Bonner Hall.\nGo through the plaza and head up the stairs.\nGo between Baldy Hall and O\'Brian Hall and you should see Jacobs Management Center across the street.';
+							break;
+						case "su":
+							label.textContent = 'Exit the building and cross the street toward Bonner Hall.\nTurn left and follow the sidewalk.\nStudent Union should be right ahead.';
+							break;
+					}
+					break;
+			}
+		}
+
+		//ACCESSIBLE PATH DIRECTIONS
+		if ($('input[name=type]:checked').val() == "accessible-o") {
+			switch (start){
+				case "cook":
+					switch (end){
+						case "jacobs":
+							label.textContent = 'Walk to Hochstetter Hall.\nExit the building by the entrance closest to Flint loop.\nGo straight along Mary Talbert Way heading towards Lockwood Library.\nCross the road to the other side of Mary Talber way and keep on going straight.\nJacobs should be right ahead.';
+							break;
+						case "su":
+							label.textContent = 'Walk to Hochstetter Hall.\nExit the building by entrance closest to Flint loop.\nGo straight between O\'Brian Hall and Capen.\nKeep walking until you pass Student Union and reach Lee Road.\nTurn left on Lee Road.\nTurn right and Student Union is to your left.';
+							break;
+						case "davis":
+							label.textContent = 'Walk to Hochstetter Hall.\nExit the building by entrance closest to Flint loop.\nGo straight between O\'Brian Hall and Capen.\nKeep walking until you pass Student Union and reach Lee road.\nTurn left and follow Lee road until you reach Bell Hall.\nGo straight pass Furnas Hall.\nTurn right and Davis should be straight ahead.';
+							break;
+					}
+					break;
+				case "jacobs":
+					switch (end){
+						case "cook":
+							label.textContent = 'Exit the building to Mary Talbert way.\nTurn left and follow Mary Talbert way.\nCooke Hall should be straight ahead.';
+							break;
+						case "su":
+							label.textContent = 'Exit the building to Mary Talbert way.\nTurn right, go straight until you pass Lockwood Library and reach Lee road.\nTurn left and follow Lee road.\nStudent Union is to your right.';
+							break;
+						case "davis":
+							label.textContent = 'Exit the building to Mary Talbert way.\nTurn right, go straight until you pass Lockwood Library and reach Lee road.\nTurn left and follow Lee road until you reach Bell Hall.\nTurn right and go straight.\nDavis should be straight ahead.';
+							break;
+					}
+					break;
+				case "su":
+					switch (end){
+						case "cook":
+							label.textContent = 'Exit the building by the entrace heading to The Commons.\nTurn right and follow Lee road until you reach Lockwood Library.\nTurn right and keep walking straight.\nCooke Hall should be straight ahead.';
+							break;
+						case "jacobs":
+							label.textContent = 'Exit the building by the entrace heading to The Commons.\nTurn right and follow Lee road until you reach Lockwood Library.\nTurn right and Jacobs should be straight ahead.';
+							break;
+						case "davis":
+							label.textContent = 'Exit the building by the entrance heading to The Commons.\nTurn left and follow Lee road until you pass Bell Hall.\nTurn right and Davis should be straight ahead.';
+							break;
+					}
+					break;
+				case "davis":
+					switch (end){
+						case "cook":
+							label.textContent = 'Exit the building by the front entrance of Davis.\nGo straight to Mary Talbert way.\nTurn left and follow Lee road until you pass Student Union.\nTurn right and keep on going straight.\nCooke Hall should be right ahead.';
+							break;
+						case "jacobs":
+							label.textContent = ' Exit the building by the front entrance of Davis.\nGo straight to Mary Talbert way.\nTurn left and follow Lee road until you pass Lockwood Library.\nTurn right to Mary Talbert way.\nJacobs should be straight ahead.';
+							break;
+						case "su":
+							label.textContent = 'Exit the building by the front entrance of Davis.\nGo straight to Mary Talbert way.\nTurn left and follow Lee road until you reach Student Union.\nStudent Union should be to your right.';
+							break;
+					}
+					break;
+			}
+		}
 		console.log("DASDAS")
 		document.getElementById("gps").style.visibility="visible"; 	
 	});
